bddea09e8747f28560f6ed5f22044b3e153106d1
roenke <roenke54@gmail.com>
2016-10-17 00:11:58 +0300

    Add tests for BaseDirectoryCommandTest

---
+++ cli/src/test/java/ru/spbau/bocharov/cli/commands/BaseDirectoryCommandTest.java
@@ -0,0 +1,43 @@
+package ru.spbau.bocharov.cli.commands;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class BaseDirectoryCommandTest {
+  @Rule
+  public TemporaryFolder folder = new TemporaryFolder();
+
+  @Test
+  public void shouldCheckThatExists() throws IOException {
+    final File file = folder.newFile("file");
+    BaseDirectoryCommand.checkExists(file.toPath());
+  }
+
+  @Test(expected = FileNotFoundException.class)
+  public void shouldCheckThatNotExists() throws FileNotFoundException {
+    BaseDirectoryCommand.checkExists(folder.getRoot().toPath().resolve("file"));
+  }
+
+  @Test
+  public void shouldCheckThatIsDirectory() throws IOException {
+    final File directory = folder.newFolder();
+    assertTrue(directory.exists());
+    assertTrue(directory.isDirectory());
+    BaseDirectoryCommand.checkDirectory(directory.toPath());
+  }
+
+  @Test(expected = FileNotFoundException.class)
+  public void shouldCheckThatIsNotDirectory() throws IOException {
+    final File file = folder.newFile("file");
+    assertTrue(file.exists());
+    assertFalse(file.isDirectory());
+    BaseDirectoryCommand.checkDirectory(file.toPath());
+  }
+}
