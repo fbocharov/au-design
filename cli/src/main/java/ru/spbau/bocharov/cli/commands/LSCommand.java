7ba8034040b05b49156b53d13c82e56883358ce3
roenke <roenke54@gmail.com>
2016-10-16 23:57:23 +0300

    Fix small bug in ls and inherit it from the BaseDirectoryCommand class

--- cli/src/main/java/ru/spbau/bocharov/cli/commands/LSCommand.java
+++ cli/src/main/java/ru/spbau/bocharov/cli/commands/LSCommand.java
@@ -14,8 +14,7 @@ import java.util.stream.Collectors;
 /**
  * @author Vitaliy.Bibaev
  */
-public class LSCommand extends BaseCommand {
-  private static final String JVM_DIRECTORY_PROPERTY = "user.dir";
+public class LSCommand extends BaseDirectoryCommand {
   private static final String FILES_SEPARATOR = "  ";
 
   /**
@@ -28,50 +27,35 @@ public class LSCommand extends BaseCommand {
   @Override
   public void execute(IO io, Context context) throws Exception {
     PrintStream stdout = new PrintStream(io.STDOUT);
-    final String currentDirectory = System.getProperty(JVM_DIRECTORY_PROPERTY);
+    final String currentDirectory = System.getProperty(Context.JVM_DIRECTORY_PROPERTY);
 
-    Path root = Paths.get(currentDirectory);
+    Paths.get(currentDirectory);
+    Path root = new File(currentDirectory).toPath().toAbsolutePath();
     if (arguments.isEmpty()) {
-      printDirectoryContent(root, false, stdout);
+      printDirectoryContent(root, root, false, stdout);
     } else {
       List<Path> directories = new ArrayList<>();
 
       for (String relativePath : arguments) {
-        final File directory = root.resolve(relativePath).toFile();
+        final Path directory = root.resolve(relativePath);
 
         checkExists(directory);
         checkDirectory(directory);
-        directories.add(root.relativize(directory.toPath()));
+        directories.add(directory);
       }
 
       for (Path directory : directories) {
-        printDirectoryContent(directory, directories.size() > 1, stdout);
+        printDirectoryContent(root, directory, directories.size() > 1, stdout);
       }
     }
   }
 
-  private void checkExists(File file) throws FileNotFoundException {
-    if (!file.exists()) {
-      String errorMessage = String.format("directory not found: %s",
-          file.toPath().toAbsolutePath().toString());
-      throw new FileNotFoundException(errorMessage);
-    }
-  }
-
-  private void checkDirectory(File directory) throws FileNotFoundException {
-    if (!directory.isDirectory()) {
-      String errorMessage = String.format("Argument should be a directory: %s",
-          directory.toPath().toAbsolutePath().toString());
-      throw new FileNotFoundException(errorMessage);
-    }
-  }
-
-  private static void printDirectoryContent(Path directory, boolean printDirectoryName, PrintStream writer)
+  private static void printDirectoryContent(Path root, Path directory, boolean printDirectoryName, PrintStream writer)
       throws IOException {
     final File[] files = directory.toFile().listFiles();
     if (files != null && files.length > 0) {
       if (printDirectoryName) {
-        writer.println(String.format("%s:", directory.toString()));
+        writer.println(String.format("%s:", root.relativize(directory).toString()));
         writer.print("\t");
       }
 
